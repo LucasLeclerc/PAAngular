00a0745443dcf4f9a9b73aacc4fab85a916ba9b3
vemoule <vemoule@users.noreply.github.com>
2022-07-19 02:31:36 +0200

    heroku

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/burger-esgi'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/burger-esgi/'}
+  );
+  });
+app.listen(4200 || 8080);
